1191d8e2cd21d6dd46279d05e4f0f10df9f7a83e
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2023-06-08 23:00:24 +0100

    fixed registrar import

--- ios/Classes/SafeDevicePlugin.m
+++ ios/Classes/SafeDevicePlugin.m
@@ -13,14 +13,14 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  }else if ([@"isJailBroken" isEqualToString:call.method]) {
+  }else if ([@"isRooted" isEqualToString:call.method]) {
     result([NSNumber numberWithBool:[self isJailBroken]]);
   }else if ([@"canMockLocation" isEqualToString:call.method]) {
     //For now we have returned if device is Jail Broken or if it's not real device. There is no
     //strong detection of Mock location in iOS
     result([NSNumber numberWithBool:([self isJailBroken] || ![self isRealDevice])]);
-  }else if ([@"isRealDevice" isEqualToString:call.method]) {
-    result([NSNumber numberWithBool:[self isRealDevice]]);
+  }else if ([@"isEmulator" isEqualToString:call.method]) {
+    result([NSNumber numberWithBool: ![self isRealDevice]]);
   } else {
     result(FlutterMethodNotImplemented);
   }
